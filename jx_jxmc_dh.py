177999256d8b529a61ba65c9be7a7e41585a7ca3
Chen <1437260988@qq.com>
2022-04-10 11:33:43 +0800

    jxmc兑换红包

---
+++ jx_jxmc_dh.py
@@ -0,0 +1,154 @@
+"""
+cron: 50 59 * * * *
+new Env('财富岛兑换红包');
+"""
+import os
+import re
+import time
+import json
+import datetime
+import requests
+from requests import RequestException
+from ql_util import get_random_str
+from ql_api import get_envs, disable_env, post_envs, put_envs
+
+# 默认配置(看不懂代码也勿动)
+cfd_start_time = -0.15
+cfd_offset_time = 0.01
+
+# 基础配置勿动
+cfd_url = 'https://m.jingxi.com/jxmc/operservice/ExchangeV2?channel=7&sceneid=1001&activeid=jxmc_active_0001&activekey=null&eid=eidAcffe812237s6uFl6N387QrWZ82DrhGUS3rhZe7doM2iz0rdd7dIcexSkrHpwu%2FY1IzR2DdVHI9bWfIvbd1k34BoeIH6Uh1yWxxksM8J1TEXOwcVs&fp=733ab47a280564bf7731f8085b33062b&dv=5.23.2&do=android&dov=11&dn=GM1910&type=11&token=5704dc54e5588f5b8aa8941860a4e50e&value=1466&jxmc_jstoken=d80e24d412425da33c977892ca624404&timestamp=1649520033869&phoneid=e5a469867a4e59f7&_stk=activeid%2Cactivekey%2Cchannel%2Cdn%2Cdo%2Cdov%2Cdv%2Ceid%2Cfp%2Cjxmc_jstoken%2Cphoneid%2Csceneid%2Ctimestamp%2Ctoken%2Ctype%2Cvalue&_ste=1&h5st=20220410000033903%3B0291573819226684%3B00df8%3Btk02w97e51b6918nPyXI0Z9wPF4eAGu26lulpbnvWKPVWEGVjTpEdgF87AM9rUy5hUGeGIUaaNGy5X99xMOY15CeG8er%3B005ada6bf041480bdbee939670bc807e3b07bedc96ccbcdbc2f0c0aab682a77c%3B3.0%3B1649520033903&_=1649520033904&sceneval=2&g_login_type=1&callback=jsonpCBKO&g_ty=ls'
+pattern_pin = re.compile(r'pt_pin=([\w\W]*?);')
+pattern_data = re.compile(r'\(([\w\W]*?)\)')
+
+
+# 获取下个整点和时间戳
+def get_date() -> str and int:
+    # 当前时间
+    now_time = datetime.datetime.now()
+    # 把根据当前时间计算下一个整点时间戳
+    integer_time = (now_time + datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:00:00")
+    time_array = time.strptime(integer_time, "%Y-%m-%d %H:%M:%S")
+    time_stamp = int(time.mktime(time_array))
+    return integer_time, time_stamp
+
+
+# 获取要执行兑换的cookie
+def get_cookie():
+    ck_list = []
+    pin = "null"
+    cookie = None
+    cookies = get_envs("JXMC_COOKIE")
+    for ck in cookies:
+        if ck.get('status') == 0:
+            ck_list.append(ck)
+    if len(ck_list) >= 1:
+        cookie = ck_list[0]
+        re_list = pattern_pin.search(cookie.get('value'))
+        if re_list is not None:
+            pin = re_list.group(1)
+        print('共配置{}条CK,已载入用户[{}]'.format(len(ck_list), pin))
+    else:
+        print('共配置{}条CK,请添加环境变量,或查看环境变量状态'.format(len(ck_list)))
+    return pin, cookie
+
+
+# 获取配置参数
+def get_config():
+    start_dist = {}
+    start_times = get_envs("JXMC_START_TIME")
+    if len(start_times) >= 1:
+        start_dist = start_times[0]
+        start_time = float(start_dist.get('value'))
+        print('从环境变量中载入时间变量[{}]'.format(start_time))
+    else:
+        start_time = cfd_start_time
+        u_data = post_envs('JXMC_START_TIME', str(start_time), '财富岛兑换时间配置,自动生成,勿动')
+        if len(u_data) == 1:
+            start_dist = u_data[0]
+        print('从默认配置中载入时间变量[{}]'.format(start_time))
+    return start_time, start_dist
+
+
+# 抢购红包请求函数
+def cfd_qq(def_start_time):
+    # 进行时间等待,然后发送请求
+    end_time = time.time()
+    while end_time < def_start_time:
+        end_time = time.time()
+    # 记录请求时间,发送请求
+    t1 = time.time()
+    d1 = datetime.datetime.now().strftime("%H:%M:%S.%f")
+    res = requests.get(cfd_url, headers=headers)
+    t2 = time.time()
+    # 正则对结果进行提取
+    re_list = pattern_data.search(res.text)
+    # 进行json转换
+    data = json.loads(re_list.group(1))
+    msg = data
+    # 根据返回值判断
+    if data == 0:
+        # 抢到了
+        msg = "可能抢到了"
+        put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
+        disable_env(u_cookie.get('_id'))
+    elif data == 2016:
+        # 需要减
+        start_time = float(u_start_time) - float(cfd_offset_time)
+        put_envs(u_start_dist.get('_id'), u_start_dist.get('name'), str(start_time)[:8])
+    elif data == 2013:
+        # 需要加
+        start_time = float(u_start_time) + float(cfd_offset_time)
+        put_envs(u_start_dist.get('_id'), u_start_dist.get('name'), str(start_time)[:8])
+    elif data == 1014:
+        # URL过期
+        pass
+    elif data == 2007:
+        # 财富值不够
+        put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
+        disable_env(u_cookie.get('_id'))
+    elif data == 9999:
+        # 账号过期
+        put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
+        disable_env(u_cookie.get('_id'))
+    print("实际发送[{}]\n耗时[{:.3f}]\n用户[{}]\n结果[{}]".format(d1, (t2 - t1), u_pin, msg))
+
+
+if __name__ == '__main__':
+    print("- 程序初始化")
+    print("脚本进入时间[{}]".format(datetime.datetime.now().strftime("%H:%M:%S.%f")))
+    # 从环境变量获取url,不存在则从配置获取
+    u_url = os.getenv("CFD_URL", cfd_url)
+    # 获取cookie等参数
+    u_pin, u_cookie = get_cookie()
+    # 获取时间等参数
+    u_start_time, u_start_dist = get_config()
+    # 预计下个整点为
+    u_integer_time, u_time_stamp = get_date()
+    print("抢购整点[{}]".format(u_integer_time))
+    print("- 初始化结束\n")
+
+    print("- 主逻辑程序进入")
+    UA = "jdpingou;iPhone;5.11.0;15.1.1;{};network/wifi;model/iPhone13,2;appBuild/100755;ADID/;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/22;pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148".format(
+        get_random_str(45, True))
+    if u_cookie is None:
+        print("未读取到JXMC_COOKIE,程序结束")
+    else:
+        headers = {
+            "Host": "m.jingxi.com",
+            "Accept": "*/*",
+            "Connection": "keep-alive",
+            'Cookie': u_cookie['value'],
+            "User-Agent": UA,
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Referer": "https://st.jingxi.com/",
+            "Accept-Encoding": "gzip, deflate, br"
+        }
+        u_start_sleep = float(u_time_stamp) + float(u_start_time)
+        print("预计发送时间为[{}]".format(datetime.datetime.fromtimestamp(u_start_sleep).strftime("%H:%M:%S.%f")))
+        if u_start_sleep - time.time() > 300:
+            print("离整点时间大于5分钟,强制立即执行")
+            cfd_qq(0)
+        else:
+            cfd_qq(u_start_sleep)
+    print("- 主逻辑程序结束")
